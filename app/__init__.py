356c46ad8ca793128bf4d4fe0b9d2b23f2cb376d
Jagadish <bjnayak333@gmail.com>
2024-12-25 23:49:44 +0530

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,16 @@
 from flask import Flask
 from .routes import create_routes
 
+from flask_mail import Mail
 app = Flask(__name__)
 app.config.from_object('config')  # Loads settings from config.py
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'dummytestnoreply01@gmail.com'
+app.config['MAIL_PASSWORD'] = 'nkvv pgvw vzrt desq'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True   
 
 # Initialize routes
 create_routes(app)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 # app/routes.py
-from flask import render_template
+from flask import render_template ,request
+from flask_mail import Mail , Message
 
 # Avoid circular import by importing `app` inside a route function
 def create_routes(app):
+    @app.route('/home')
     @app.route('/')
     def index():
         return render_template('index.html')
@@ -13,12 +15,34 @@ def create_routes(app):
 
     @app.route('/projects')
     def projects():
-        project_list = [
-            {'title': 'Project 1', 'description': 'Description of Project 1', 'url': '#'},
-            {'title': 'Project 2', 'description': 'Description of Project 2', 'url': '#'}
-        ]
-        return render_template('projects.html', projects=project_list)
+        return render_template('projects.html')
 
-    @app.route('/contact')
+    @app.route('/contact', methods=["GET","POST"])
     def contact():
+        if request.method == "GET":
+            return render_template("contact.html")
+        elif request.method == "POST":
+            mail = Mail(app)
+            name = request.form["name"]
+            email = request.form['email']
+            if email :
+                customer = [email]
+                print(customer)       
+                msgbody = f"""
+Hi {name},
+Thank you so much for visiting my portfolio website and taking the time to send me your feedback and suggestions! I truly appreciate your thoughts on [specific suggestion or message they sent].
+
+Your insights have given me some great ideas to consider, and I'm always looking to improve my work. If you have any further suggestions or questions, feel free to reach out—I’d love to continue the conversation.
+            
+Thanks again for your support!
+
+Best regards,
+Bukya Jagadish Naik """
+            msg = Message("Thank You for Your Feedback on My Portfolio", sender='noreplay@demo.com' ,recipients=customer)
+            msg.body = msgbody
+            mail.send(msg)
         return render_template('contact.html')
+    
+    @app.route('/services')
+    def services():
+        return render_template('services.html')
